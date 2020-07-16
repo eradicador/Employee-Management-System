7c16daedc0163b74bb46867ea2cc3151d6a6df61
eradicador <janisepinto@yahoo.com>
2020-07-16 00:31:53 -0400

    Fixed the three tables in schema file

--- schema.sql
+++ schema.sql
@@ -1,24 +1,26 @@
-DROP database if exists employee_db;
-
-CREATE database employee_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
 USE employee_db
 
-CREATE TABLE `employee` (
-  id int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE employee(
+  id INT auto_increment NOT NULL,
   first_name VARCHAR(255) NOT NULL,
   last_Name VARCHAR(255) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
   PRIMARY KEY(id)
+);
 
-) ;
-
-CREATE TABLE `role` (
-id INT(11) NOT NULL AUTO_INCREMENT,
-  title VARCHAR(255) DEFAULT NULL,
-  salary int(11) NOT NULL,
+CREATE TABLE role (
+id INT(11) AUTO_INCREMENT NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  salary INT(11) NOT NULL,
+  department_id INT NOT NULL,
   PRIMARY KEY (id)
 ); 
-CREATE TABLE `department` (
-  name VARCHAR(255) DEFAULT NULL,
-  id PRIMARY KEY (`id`),
+CREATE TABLE department (
+  id INT  auto_increment NOT null,
+  name VARCHAR(255) NOT NULL,
+  id PRIMARY KEY (id),
 );
