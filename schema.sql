40f1aca6c4703e8d32da9a4eb942390cce020e5a
eradicador <janisepinto@yahoo.com>
2020-07-16 00:49:26 -0400

    added and fixed tables to schema.sql

--- schema.sql
+++ schema.sql
@@ -24,3 +24,18 @@ CREATE TABLE department (
   name VARCHAR(255) NOT NULL,
   id PRIMARY KEY (id),
 );
+
+select * from employee;
+select * from role;
+select * from department;
+
+INSERT into department (name)
+VALUES ("Sales");
+INSERT into department (name)
+VALUES ("Engineering");
+INSERT into department (name)
+VALUES ("Finance");
+INSERT into department (name)
+VALUES ("Legal");
+INSERT into department (name)
+VALUES ("Manager");
